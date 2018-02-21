eda2ce709b67e692f271624bd9ab2750da4d2391
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-02-22 01:59:49 +0400

    Upd

---
+++ docker-toolbox/tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'docker-toolbox'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '$toolsDir\DockerToolbox.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+
+  File           = $url
+  softwareName  = 'docker-toolbox*'
+
+  silentArgs    = "/COMPONENTS=`"docker,dockermachine,dockercompose`" /TASKS=`"modifypath,upgradevm`" /silent /norestart /log=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
